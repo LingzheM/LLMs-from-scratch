2447c862df05ecd51e58a8830ccc616b9186f8df
LingZhe <1119538589@qq.com>
2025-06-26 22:33:26 +0900

    ch06

---
+++ pkg/llms_from_scratch/ch06.py
@@ -0,0 +1,73 @@
+import urllib.request
+import zipfile
+import os
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+import torch
+import pandas as pd
+
+def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
+    if data_file_path.exist():
+        print(f"{data_file_path} already exists. Skipping download and extraction.")
+        return 
+    
+    with urllib.request.urlopen(url) as response:
+        with open(zip_path, "wb") as out_file:
+            out_file.write(response.read())
+
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extracted_path)
+
+    original_file_path = Path(extracted_path)  / "SMSSpamCollection"
+    os.rename(original_file_path, data_file_path)
+    print(f"File downloaded and saved as {data_file_path}")
+
+
+def create_balanced_dataset(df):
+    num_spam = df[df["Label"] == "spam"].shape[0]
+
+    ham_subset = df[df["Label"] == "ham"].sample(num_spam, random_state=123)
+
+    balanced_df = pd.concat([ham_subset, df[df["Label"] == "span"]])
+
+    return balanced_df
+
+def random_split(df, train_frac, validation_frac):
+    df = df.sample(frac=1, random_state=123).reset_index(drop=True)
+
+    train_end = int(len(df) * train_frac)
+
+    validation_end = train_end + int(len(df) * validation_frac)
+
+    train_df = df[:train_end]
+    validation_df = df[train_end:validation_end]
+    test_df = df[validation_end:]
+
+    return train_df, validation_df, test_df
+
+
+class SpamDataset(Dataset):
+    def __init__(self, csv_file, tokenizer, max_length=None, pad_token_id=50256):
+        super().__init__()
+
+        self.encoded_texts = [
+            tokenizer.encode(text) for text in self.data["Text"]
+        ]
+
+        if max_length is None:
+            self.max_length = self._longest_encoded_length
+        else:
+            self.max_length = max_length
+            self.encoded_texts = [
+                encoded_text[:self.max_length]
+                for encoded_text in self.encoded_texts
+            ]
+        
+        self.encoded_texts = [
+            encoded_text + [pad_token_id] * (self.max_length - len(encoded_text))
+            for encoded_text in self.encoded_texts
+        ]
+
+        
